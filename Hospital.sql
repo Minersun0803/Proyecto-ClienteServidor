b302308d38eba28a3ea3698eb65e36d8d3378650
Minersun0803 <ecorralesamador@gmail.com>
2026-03-16 09:12:52 -0600

    Cambios del SQL

--- Hospital.sql
+++ Hospital.sql
@@ -1,3 +1,39 @@
 CREATE DATABASE Hospital;
 
 USE Hospital;
+
+CREATE TABLE Person (
+    PersonID INT PRIMARY KEY AUTO_INCREMENT,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Cedula INT NOT NULL UNIQUE,
+    Telefono VARCHAR(15),
+    Correo VARCHAR(100),
+    Direccion VARCHAR(255),
+    Contraseña VARCHAR(255) NOT NULL,
+);
+
+
+CREATE TABLE Inventario (ProductoID INT PRIMARY KEY AUTO_INCREMENT, 
+    Nombre VARCHAR(100) NOT NULL, 
+    Descripcion TEXT, 
+    Cantidad INT NOT NULL
+);
+
+CREATE TABLE Provedor (ProvedorID INT PRIMARY KEY AUTO_INCREMENT, 
+    Nombre VARCHAR(100) NOT NULL, 
+    Telefono VARCHAR(15), 
+    Correo VARCHAR(100), 
+    Direccion VARCHAR(255)
+);
+
+CREATE TABLE Cita (
+    CitaID INT PRIMARY KEY AUTO_INCREMENT,
+    PacienteID INT,
+    MedicoID INT,
+    Fecha DATETIME NOT NULL,
+    Motivo TEXT,
+    FOREIGN KEY (PacienteID) REFERENCES Person(PersonID),
+    FOREIGN KEY (MedicoID) REFERENCES Person(PersonID)
+);
+
--- src/main/java/com/mycompany/proyecto/Medico.java
+++ src/main/java/com/mycompany/proyecto/Medico.java
@@ -4,6 +4,6 @@ public class Medico extends Persona {
     
     public Medico(int id, String Nombre, String Apellidos, String Cedula, String Telefono, String Correo, String Ubicacion) {
         super(id, Nombre, Apellidos, Cedula, Telefono, Correo, Ubicacion);
-    }// hola como estan
+    }
 
 }
