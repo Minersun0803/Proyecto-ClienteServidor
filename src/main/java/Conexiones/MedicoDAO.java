2c63d8f0e616656d8d22c8e6f365bf05c68f7d2f
Minersun0803 <ecorralesamador@gmail.com>
2026-04-08 14:34:17 -0600

    Correciones

--- src/main/java/Conexiones/MedicoDAO.java
+++ src/main/java/Conexiones/MedicoDAO.java
@@ -45,111 +45,111 @@ public class MedicoDAO {
         return lista;
     }
 
-    public boolean AgregarMedico(Medico medico) {
-        ConexionSQL conexionSQL = new ConexionSQL();
+//    public boolean AgregarMedico(Medico medico) {
+//        ConexionSQL conexionSQL = new ConexionSQL();
+//
+//        try {
+//            // Inserta primero en Persona datos generales.
+//            String sqlPerson = "INSERT INTO Person (FirstName, LastName, Cedula, Telefono, Correo, Direccion, Contraseña, AñoNacimiento)"
+//                    + "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+//            //Luego inserta en medico datos especificos.
+//            String sqlMedico = "INSERT INTO Medico(PersonaID, Especialidad)" + "VALUES(?,?)";
+//
+//            PreparedStatement psPerson = conexionSQL.conectarSQL().prepareStatement(sqlPerson, Statement.RETURN_GENERATED_KEYS);
+//            PreparedStatement psMedico = conexionSQL.conectarSQL().prepareStatement(sqlMedico);
+//
+//            //Insertar datos del medico
+//            psPerson.setString(1, medico.getNombre());
+//            psPerson.setString(2, medico.getApellidos());
+//            psPerson.setString(3, medico.getCedula());
+//            psPerson.setString(4, medico.getTelefono());
+//            psPerson.setString(5, medico.getCorreo());
+//            psPerson.setString(6, medico.getUbicacion());
+//
+//            psMedico.executeUpdate();
+//
+//            ResultSet rs = psPerson.getGeneratedKeys();
+//            if (rs.next()) {
+//                int personID = rs.getInt(1);
+//                // Insertar en Paciente
+//                psMedico.setInt(1, personID);
+//                psMedico.executeUpdate();
+//            }
+//
+//            return true;
+//
+//        } catch (Exception ex) {
+//            System.out.println("Exception Listar Medicos: " + ex.getMessage());
+//        } finally {
+//            conexionSQL.desconectarSQL();
+//        }
+//        return false;
+//    }
 
-        try {
-            // Inserta primero en Persona datos generales.
-            String sqlPerson = "INSERT INTO Person (FirstName, LastName, Cedula, Telefono, Correo, Direccion, Contraseña, AñoNacimiento)"
-                    + "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-            //Luego inserta en medico datos especificos.
-            String sqlMedico = "INSERT INTO Medico(PersonaID, Especialidad)" + "VALUES(?,?)";
+//    public boolean EditarMedico(Medico medico) {
+//        ConexionSQL conexionSQL = new ConexionSQL();
+//
+//        try {
+//            String sqlPersona = """
+//                         UPDATE Persona SET
+//                         FirstName  = ?,
+//                         LastName  = ?,
+//                         Telefono   = ?,
+//                         Correo   = ?,
+//                         Especialidad = ?
+//                         WHERE codigo = ?
+//                         """;
+//            String sqlMedico = """
+//                              UPDATE Medico SET
+//                              Especialidad = ?
+//                              WHERE MedicoID= ?
+//                              """;
+//
+//            PreparedStatement psPersona = conexionSQL.conectarSQL().prepareStatement(sqlPersona, Statement.RETURN_GENERATED_KEYS);
+//            PreparedStatement psMedico = conexionSQL.conectarSQL().prepareStatement(sqlMedico);
+//
+//            psPersona.setString(1, medico.getNombre());
+//            psPersona.setString(1, medico.getNombre());
+//            psPersona.setString(2, medico.getApellidos());
+//            psPersona.setString(3, medico.getCedula());
+//            psPersona.setString(4, medico.getTelefono());
+//            psPersona.setString(5, medico.getCorreo());
+//            psPersona.setString(6, medico.getUbicacion());
+//            psPersona.setInt(7, medico.getMedicoID());
+//
+//            psPersona.executeUpdate();
+//            return true;
+//
+//        } catch (Exception ex) {
+//            System.out.println("Exception Listar Medicos: " + ex.getMessage());
+//        } finally {
+//            conexionSQL.desconectarSQL();
+//        }
+//        return false;
+//
+//    }
 
-            PreparedStatement psPerson = conexionSQL.conectarSQL().prepareStatement(sqlPerson, Statement.RETURN_GENERATED_KEYS);
-            PreparedStatement psMedico = conexionSQL.conectarSQL().prepareStatement(sqlMedico);
-
-            //Insertar datos del medico
-            psPerson.setString(1, medico.getNombre());
-            psPerson.setString(2, medico.getApellidos());
-            psPerson.setString(3, medico.getCedula());
-            psPerson.setString(4, medico.getTelefono());
-            psPerson.setString(5, medico.getCorreo());
-            psPerson.setString(6, medico.getUbicacion());
-
-            psMedico.executeUpdate();
-
-            ResultSet rs = psPerson.getGeneratedKeys();
-            if (rs.next()) {
-                int personID = rs.getInt(1);
-                // Insertar en Paciente
-                psMedico.setInt(1, personID);
-                psMedico.executeUpdate();
-            }
-
-            return true;
-
-        } catch (Exception ex) {
-            System.out.println("Exception Listar Medicos: " + ex.getMessage());
-        } finally {
-            conexionSQL.desconectarSQL();
-        }
-        return false;
-    }
-
-    public boolean EditarMedico(Medico medico) {
-        ConexionSQL conexionSQL = new ConexionSQL();
-
-        try {
-            String sqlPersona = """
-                         UPDATE Persona SET
-                         FirstName  = ?,
-                         LastName  = ?,
-                         Telefono   = ?,
-                         Correo   = ?,
-                         Especialidad = ?
-                         WHERE codigo = ?
-                         """;
-            String sqlMedico = """
-                              UPDATE Medico SET
-                              Especialidad = ?
-                              WHERE MedicoID= ?
-                              """;
-
-            PreparedStatement psPersona = conexionSQL.conectarSQL().prepareStatement(sqlPersona, Statement.RETURN_GENERATED_KEYS);
-            PreparedStatement psMedico = conexionSQL.conectarSQL().prepareStatement(sqlMedico);
-
-            psPersona.setString(1, medico.getNombre());
-            psPersona.setString(1, medico.getNombre());
-            psPersona.setString(2, medico.getApellidos());
-            psPersona.setString(3, medico.getCedula());
-            psPersona.setString(4, medico.getTelefono());
-            psPersona.setString(5, medico.getCorreo());
-            psPersona.setString(6, medico.getUbicacion());
-            psPersona.setInt(7, medico.getMedicoID());
-
-            psPersona.executeUpdate();
-            return true;
-
-        } catch (Exception ex) {
-            System.out.println("Exception Listar Medicos: " + ex.getMessage());
-        } finally {
-            conexionSQL.desconectarSQL();
-        }
-        return false;
-
-    }
-
-    public boolean EliminarMedico(int codigo) {
-        ConexionSQL conexionSQL = new ConexionSQL();
-
-        try {
-            String sqlPerson = "DELETE FROM Persona WHERE PersonID = ?";
-            String sqlMedico = "DELETE FROM Medico WHERE MedicoID = ?";
-
-            PreparedStatement psPerson = conexionSQL.conectarSQL().prepareStatement(sqlPerson);
-            PreparedStatement psMedico = conexionSQL.conectarSQL().prepareStatement(sqlMedico);
-            psPerson.setInt(1, codigo);
-            psMedico.setInt(1, codigo);
-
-            psPerson.executeUpdate();
-            psMedico.executeUpdate();
-            return true;
-
-        } catch (Exception ex) {
-            System.out.println("Exception Listar Medicos: " + ex.getMessage());
-        } finally {
-            conexionSQL.desconectarSQL();
-        }
-        return false;
-    }
+//    public boolean EliminarMedico(int codigo) {
+//        ConexionSQL conexionSQL = new ConexionSQL();
+//
+//        try {
+//            String sqlPerson = "DELETE FROM Persona WHERE PersonID = ?";
+//            String sqlMedico = "DELETE FROM Medico WHERE MedicoID = ?";
+//
+//            PreparedStatement psPerson = conexionSQL.conectarSQL().prepareStatement(sqlPerson);
+//            PreparedStatement psMedico = conexionSQL.conectarSQL().prepareStatement(sqlMedico);
+//            psPerson.setInt(1, codigo);
+//            psMedico.setInt(1, codigo);
+//
+//            psPerson.executeUpdate();
+//            psMedico.executeUpdate();
+//            return true;
+//
+//        } catch (Exception ex) {
+//            System.out.println("Exception Listar Medicos: " + ex.getMessage());
+//        } finally {
+//            conexionSQL.desconectarSQL();
+//        }
+//        return false;
+//    }
 }
